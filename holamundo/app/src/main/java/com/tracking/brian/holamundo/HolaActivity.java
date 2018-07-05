71b6e8ac3acc8b228c28410d22107eee2f63b372
Brian Vega <dentvega6@gmail.com>
2018-07-04 22:25:55 -0500

    HolaMundo

---
+++ holamundo/app/src/main/java/com/tracking/brian/holamundo/HolaActivity.java
@@ -0,0 +1,13 @@
+package com.tracking.brian.holamundo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HolaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola);
+    }
+}
