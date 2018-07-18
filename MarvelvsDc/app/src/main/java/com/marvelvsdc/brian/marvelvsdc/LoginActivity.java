f2a1fa52ebfeafe71dbc93c523c1e1a13e9c2ec1
Brian Vega <dentvega6@gmail.com>
2018-07-17 20:59:58 -0500

    Proyecto marvel vs dc

--- MarvelvsDc/app/src/main/java/com/marvelvsdc/brian/marvelvsdc/LoginActivity.java
+++ MarvelvsDc/app/src/main/java/com/marvelvsdc/brian/marvelvsdc/LoginActivity.java
@@ -1,29 +1,57 @@
 package com.marvelvsdc.brian.marvelvsdc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edtEmail;
+    EditText edtContraseña;
+    Button btnIngresar;
+    Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        edtEmail = findViewById(R.id.edt_email);
+        edtContraseña = findViewById(R.id.edt_passowrd);
+        btnIngresar = findViewById(R.id.btn_ingresar);
+        btnRegistrar = findViewById(R.id.btn_registrar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                String email = edtEmail.getText().toString();
+                String contraseña = edtContraseña.getText().toString();
+
+                if (email.length()>5 && contraseña.length() > 5){
+                    UtilNavigationiActivity.goToActivityFins(LoginActivity.this, SettigActivity.class);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Email= " + email + ", Constraseña= " +contraseña , Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtEmail.setText("Cambio de texto");
+                edtContraseña.setText("987654321");
+            }
+        });
+
     }
 
 }
---
+++ MarvelvsDc/app/src/main/java/com/marvelvsdc/brian/marvelvsdc/UtilNavigationiActivity.java
@@ -0,0 +1,18 @@
+package com.marvelvsdc.brian.marvelvsdc;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class UtilNavigationiActivity {
+
+    public static void goToActivity(Activity activity, Class classs){
+        Intent intent = new Intent(activity, classs);
+        activity.startActivity(intent);
+    }
+
+    public static void goToActivityFins(Activity activity, Class classGoto){
+        Intent intent = new Intent(activity, classGoto);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+}
