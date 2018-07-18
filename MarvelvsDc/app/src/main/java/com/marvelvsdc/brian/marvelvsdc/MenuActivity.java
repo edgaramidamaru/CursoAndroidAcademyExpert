36f388783d9f72ec5d790ea0073e7645d0f5a7f8
Brian Vega <dentvega6@gmail.com>
2018-07-17 21:03:37 -0500

    Proyecto marvel vs dc

--- MarvelvsDc/app/src/main/java/com/marvelvsdc/brian/marvelvsdc/MenuActivity.java
+++ MarvelvsDc/app/src/main/java/com/marvelvsdc/brian/marvelvsdc/MenuActivity.java
@@ -23,15 +23,6 @@ public class MenuActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -40,6 +31,10 @@ public class MenuActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
     }
 
     @Override
