3001092926133091f729836347baa6aff43a07a6
Brian Vega <dentvega6@gmail.com>
2018-08-07 19:32:56 -0500

    retrofit

---
+++ Podcast/app/src/main/java/com/questionary/brian/podcast/Retrofit/Usuarios/JsonPlaceHolderApiService.java
@@ -0,0 +1,37 @@
+package com.questionary.brian.podcast.Retrofit.Usuarios;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Battl on 31/07/2017.
+ */
+
+public class JsonPlaceHolderApiService {
+
+    public static JsonPlaceService API_SERVICE;
+
+    public static JsonPlaceService getRetrofitUser(){
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder httcliente = new OkHttpClient.Builder();
+        httcliente.addInterceptor(loggingInterceptor);
+
+        String baseUrl = "https://jsonplaceholder.typicode.com";
+
+        if (API_SERVICE == null){
+            Retrofit retrofit = new Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(baseUrl)
+                    .build();
+            API_SERVICE = retrofit.create(JsonPlaceService.class);
+        }
+
+        return API_SERVICE;
+    }
+
+}
---
+++ Podcast/app/src/main/java/com/questionary/brian/podcast/Retrofit/Usuarios/JsonPlaceService.java
@@ -0,0 +1,38 @@
+package com.questionary.brian.podcast.Retrofit.Usuarios;
+
+
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+/**
+ * Created by Battl on 31/07/2017.
+ */
+
+public interface JsonPlaceService {
+
+//    @GET("/users")
+//    Call<List<UsuariosR>> getlistUsuarios();
+//
+//    @GET("/users/{id}")
+//    Call<UsuariosR> getUser(@Path("id") String id);
+//
+//    @GET("/posts")
+//    Call<List<Posts>> getListPosts();
+//
+//    @POST("/posts")
+//    Call<Posts> postPosts();
+//
+//    @PUT("/posts/{id}")
+//    Call<Posts> putPosts(@Path("id") String id);
+//
+//    @DELETE("/posts/{id}")
+//    Call<Posts> deletePosts(@Path("id") String id);
+//
+//    @GET("/photos")
+//    Call<List<Photos>> getListPhotos();
+
+}
